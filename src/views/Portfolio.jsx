b566bc677ccc0bf532c49c0d2a02714c9f4196a1
Patrick Lindsay <lindsaypj88@gmail.com>
2023-09-11 22:24:51 -0700

    Added portfolio page structure

--- src/views/Portfolio.jsx
+++ src/views/Portfolio.jsx
@@ -2,16 +2,26 @@ import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import TypingText from "../components/TypingText";
 
-export default function Portfolio({ handleHeadingTyped }) {
+import '../styles/Portfolio.css';
+import WebDev from "./WebDev";
+
+export default function Portfolio({ headingTypedCallback }) {
   return (
-    <Container>
-      <Row>
-        <Col>
+    <Container fluid>
+      <Row className="portfolio__header-row">
+        <Col className="p-0">
             <h1 className='route-header padding-margins'>
-              <TypingText text="/portfolio" doneTypingCallback={handleHeadingTyped} />
+              <TypingText text="/portfolio" doneTypingCallback={headingTypedCallback} />
             </h1>
+
+            <h2 className='text-center'>Hero section for portfolio</h2>
         </Col>
       </Row>
+
+      {/* Web Development */}
+      <WebDev />
+
+      {/*  */}
     </Container>
   );
 };
---
+++ src/views/WebDev.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+
+export default function WebDev({  }) {
+  return (
+    <Row className='bg-black bg-opacity-75'>
+      <Col className='p-0'>
+        <h1 className='route-header padding-margins'>/web_development</h1>
+
+        {/* REI Membership Dashboard */}
+        <Row className='web-dev__row'>
+          <Col className='p-0'>
+
+          </Col>
+        </Row>
+
+        {/* Sudoku */}
+
+        {/* Neurian */}
+      </Col>
+    </Row>
+  );
+}
