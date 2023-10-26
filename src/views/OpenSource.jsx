a439582c5ae3953b86a4f91ab7f8c95afcf4a96e
Patrick Lindsay <lindsaypj88@gmail.com>
2023-10-26 12:31:33 -0700

    Added links to Flutter project

--- src/views/OpenSource.jsx
+++ src/views/OpenSource.jsx
@@ -28,22 +28,25 @@ export default function OpenSource() {
               <p>
                 During my coursework, I had the opportunity to learn about contributing to
                 open source projects. As a team of 4, we were paired with mentor who works at Google
-                on the Flutter Framework. Flutter is a cross platform app development framework that
-                allows you to create one app for all the major platforms. Flutter uses the dart
-                programming language, which we had to learn.
+                on the Flutter Framework. <a href="https://flutter.dev/" target='_blank' rel='noreferrer'>Flutter</a> is a cross platform app development framework that
+                allows you to create one app for all the major platforms. Flutter uses
+                the <a href="https://dart.dev/" target='_blank' rel='noreferrer'>Dart</a> programming
+                language, which we had to learn.
               </p>
               <p>
                 After getting our environments set up, reading all the documentation, and learning
-                about Dart, we started working on open issues. The issue I took on was a feature
-                request for a month/year mode to the Cupertino (Apple's design system) Date Picker.
-                I got to dive deep into the functionality and testing pipelines that exist for the
-                Date Picker and add in the new mode with its own tests.
+                about Dart, we started working on open issues. The issue I took on was a <a href="https://github.com/flutter/flutter/issues/93508" target='_blank' rel='noreferrer'>feature request</a> for
+                a month/year mode to the  Cupertino (Apple's
+                design system) Date Picker. I got to dive deep into the functionality and testing
+                pipelines that exist for the Date Picker and add in the new mode with its own tests.
+                I created a <a href="https://github.com/flutter/flutter/pull/125603" target='_blank' rel='noreferrer'>PR</a>,
+                recieved feedback, and ultimatly the changes merged!
               </p>
               <p>
-                Finally, as a group we started a design discussion to resolve some conflicting
-                issues regarding the Date Picker's functionality and how it differs from the
-                Native IOS functionallity. Our mentor presented our findings during a meeting
-                with other Googlers for feedback.
+                Finally, as a group we started a <a href="https://docs.google.com/document/d/1Ib5ztLzc19e1Uggz16BFlrJMg1TOdYLyP-WqtTpDltM/edit?resourcekey=0-11oVmOsTHLD5fqxdAHZqNg#heading=h.pub7jnop54q0#enable-force-dark" target='_blank' rel='noreferrer'>design discussion</a> to
+                resolve some conflicting issues regarding the Date Picker's functionality and how
+                it differs from the Native IOS functionallity. Our mentor presented our findings
+                during a meeting with other Googlers for feedback.
               </p>
               <div className='text-center p-4'>
                 <DartLogo className='portfolio-logo' />
