81be3ca8553a8f1a1132f15ee01dc0c8bf729bb0
Patrick Lindsay <lindsaypj88@gmail.com>
2023-11-03 22:49:07 -0700

    Converted Ascii art to img and refactored for mobile. Closes #3, closes #19.

--- src/components/ascii-portrait.jsx
+++ src/components/ascii-portrait.jsx
@@ -1,25 +1,29 @@
 import React, { useEffect, useRef } from "react";
 
 import '../styles/ascii.css';
+import AsciiImg from '../resources/images/AsciiPortrait/ascii-large.png';
 
 export default function AsciiPortrait({ visible }) {
   const frame = useRef();
 
   useEffect(() => {
     if (visible) {
-      frame.current.style.visibility = "visible";
+      frame.current.style.visibility = 'visible';
     }
     else {
-      frame.current.style.visibility = "hidden";
+      frame.current.style.visibility = 'hidden';
     }
   }, [visible]);
 
   return (
-    <iframe
-      ref={frame}
-      src="ascii-art.html"
-      className="ascii-frame2"
-      title="Ascii self-portrait"
-    ></iframe>
+    <div className='ascii-container'>
+      <img
+        ref={frame}
+        src={AsciiImg}
+        className='ascii'
+        alt='Ascii self-portrait'
+      />
+    </div>
+    
   );
 }
