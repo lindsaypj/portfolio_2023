88a0a0e29208d555a9b40dad3b459a2ec9eebffa
Patrick Lindsay <lindsaypj88@gmail.com>
2023-11-28 11:27:21 -0800

    Initial sudoku objects

---
+++ src/objects/SudokuGame.js
@@ -0,0 +1,17 @@
+import getCellGroups from "./SudokuInitTools";
+
+export class SudokuGame {
+  constructor({ size, board }) {
+    // Game data
+    this.size = size;
+    this.board = board;
+
+    // Cell conflict groups
+    const cellGroups = getCellGroups(size);
+    this.rows = cellGroups.rows;
+    this.cols = cellGroups.cols;
+    this.groups = cellGroups.groups;
+
+
+  }
+}
