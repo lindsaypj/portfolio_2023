576491cfa51012bd8ae21d1237c1c313cfe11898
Patrick Lindsay <lindsaypj88@gmail.com>
2024-03-07 16:47:12 -0800

    Changed /about_me bio animaiton to a slide-in. Closes #65

--- src/views/AboutMe.jsx
+++ src/views/AboutMe.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState, useCallback } from "react";
+import React, { useEffect, useRef } from "react";
 import useScrollPos from '../hooks/useScrollPos';
 
 import TypingText from '../components/TypingText';
@@ -12,29 +12,36 @@ import GitHubLogo from "../resources/logos/GitHubLogo";
 import LinkedInLogo from "../resources/logos/LinkedInLogo";
 
 export default function AboutMe({ headingTypedCallback, setTerminalHero, headingTyped, mobileMode }) {
-  // HOOKS
-  const textSection = useRef();
 
-  const [paragraphsTyped, setParagraphsTyped] = useState(0);
+  ////    INITIALIZATION    ////
+
+  const textSection = useRef();
   const scrollPos = useScrollPos();
 
-  /* Observers for dynamic content */
+
+  ////    STATE MANAGMENT    ////
+
+  // Show Bio when visible
   useEffect(() => {
-    /* Observer to start typing about me text when visible */
+    const textSelectionInstance = textSection.current;
     const textObserver = new IntersectionObserver((entries, observer) => {
       const trigger = entries[0];
       if (trigger.isIntersecting) {
-        setParagraphsTyped(1);
-        observer.unobserve(textSection.current);
+        showBio();
+        observer.unobserve(textSelectionInstance);
+        observer.disconnect();
       }
     });
-    textObserver.observe(textSection.current);
+    textObserver.observe(textSelectionInstance);
 
     return () => {
+      textObserver.unobserve(textSelectionInstance);
+      textObserver.disconnect();
       setTerminalHero(false);
     }
   }, [setTerminalHero]);
 
+  // Handle hero mode
   useEffect(() => {
     if (scrollPos > 0) {
       setTerminalHero(false);
@@ -43,119 +50,102 @@ export default function AboutMe({ headingTypedCallback, setTerminalHero, heading
       setTerminalHero(true);
     }
 
-    // On unmount
-    return () => {
+    return () => { // On unmount
       setTerminalHero(false);
     }
   }, [scrollPos, setTerminalHero]);
 
-  const handleParagraphTyped = useCallback(() => {
-    setParagraphsTyped(paragraphsTyped + 1);
-  }, [paragraphsTyped]);
 
-    return (
-      <>
-      <Container fluid className='p-0'>
-        {/* Hero Section */}
-        <Row className='hero-row'>
-          <Col className="p-0">
+  ////    RENDERING    ////
+
+  const showBio = () => {
+    textSection.current.classList.add('show');
+  }
+
+  return (
+    <>
+    <Container fluid className='p-0'>
+      {/* Hero Section */}
+      <Row className='hero-row'>
+        <Col className="p-0">
+        
+          <AsciiPortrait visible={headingTyped} mobileMode={mobileMode} />
+
+          <h1 className='about-me__header route-header padding-margins bg-gradient-down'>
+            <TypingText text="/about_me" doneTypingCallback={headingTypedCallback} />
+          </h1>
           
-            <AsciiPortrait visible={headingTyped} mobileMode={mobileMode} />
-
-            <h1 className='about-me__header route-header padding-margins bg-gradient-down'>
-              <TypingText text="/about_me" doneTypingCallback={headingTypedCallback} />
-            </h1>
-            
-            <div className='about-me__title padding-margins'>
-              <h3>
-                Software Engineer
-              </h3>
-              <a className='about-me__link me-2 me-md-3' href='https://github.com/lindsaypj' target='_blank' rel='noreferrer'>
-                <div className='d-inline-block'>
-                  GitHub
-                  <GitHubLogo className='ms-1' />
-                </div>
-              </a>
-              <a className='about-me__link' href='https://www.linkedin.com/in/lindsaypj/' target='_blank' rel='noreferrer'>
-                <div className='d-inline-block'>
-                  LinkedIn
-                  <LinkedInLogo className='ms-1' />
-                </div>
-              </a>
-            </div>
-
-          </Col>
-        </Row>
-
-        {/* Brief Bio */}
-        <Row className='about-me__row bg-black bg-opacity-75'>
-          <Col
-            className='p-0'
-            xs={12} xl={6} xxl={5}
-          >
-            <div className='about-me__bio padding-margins pe-xl-0'>
-              <div ref={textSection}>
-                <p>
-                  <         TypingText
-                    text='I just received my Bachelor of Applied Science in Software Development,
-                    but I am more eager than ever to keep learning.'
-                    charInterval={10}
-                    shouldType={paragraphsTyped >= 1}
-                    doneTypingCallback={handleParagraphTyped}
-                  />
-                </p>
-                <p>
-                  <TypingText
-                    text='I quickly adopt new skills and software. I am confident with Full-stack web
-                          development, database management, and object oriented programming. I love 
-                          pushing the limits of what I know.'
-                    charInterval={10}
-                    shouldType={paragraphsTyped >= 2}
-                    doneTypingCallback={handleParagraphTyped}
-                  />
-                </p>
-                <p>
-                  <TypingText
-                    text='My interests extend beyond software development to automotive and rock climbing.
-                          One exciting achievement that still inspires me to this day was converting my car
-                          from using an automatic transmission to using a manual transmission.'
-                    charInterval={10}
-                    shouldType={paragraphsTyped >= 3}
-                    doneTypingCallback={handleParagraphTyped}
-                  />
-                </p>
-                <p>
-                  <TypingText
-                    text='I am confident I can adapt to any technology, however I am currently comfortable
-                          with agile development and the following tools and languages:'
-                    charInterval={10}
-                    shouldType={paragraphsTyped >= 4}
-                  />
-                </p>
+          <div className='about-me__title padding-margins'>
+            <h3>
+              Software Engineer
+            </h3>
+            <a className='about-me__link me-2 me-md-3' href='https://github.com/lindsaypj' target='_blank' rel='noreferrer'>
+              <div className='d-inline-block'>
+                GitHub
+                <GitHubLogo className='ms-1' />
               </div>
+            </a>
+            <a className='about-me__link' href='https://www.linkedin.com/in/lindsaypj/' target='_blank' rel='noreferrer'>
+              <div className='d-inline-block'>
+                LinkedIn
+                <LinkedInLogo className='ms-1' />
+              </div>
+            </a>
+          </div>
+
+        </Col>
+      </Row>
+
+      {/* Brief Bio */}
+      <Row className='about-me__row bg-black bg-opacity-75'>
+        <Col
+          className='p-0'
+          xs={12} xl={6} xxl={5}
+        >
+          <div className='about-me__bio padding-margins pe-xl-0'>
+            <div ref={textSection}>
+              <p>
+                I just received my Bachelor of Applied Science in Software Development,
+                but I am more eager than ever to keep learning.
+              </p>
+              <p>
+                I quickly adopt new skills and software. I am confident with Full-stack web
+                development, database management, and object oriented programming. I love 
+                pushing the limits of what I know.
+              </p>
+              <p>
+                My interests extend beyond software development to automotive and rock climbing.
+                One exciting achievement that still inspires me to this day was converting my car
+                from using an automatic transmission to using a manual transmission.
+              </p>
+              <p>
+                I am confident I can adapt to any technology, however I am currently comfortable
+                with agile development and the following tools and languages:
+              </p>
             </div>
-          </Col>
-          <Col
-            className='p-0 text-center'
-            xs={12} xl={6} xxl={7}
-          >
-            <div className='about-me__details padding-margins mx-xl-auto'>
-              <div>
-                <GRCLogo />
+          </div>
+        </Col>
+        <Col
+          className='p-0 text-center'
+          xs={12} xl={6} xxl={7}
+        >
+          <div className='about-me__details padding-margins mx-xl-auto'>
+            <div>
+              <GRCLogo />
 
-                <hr></hr>
+              <hr></hr>
 
-                <div className='p-4'>
-                  {aboutMeSkills.map((skill) => (
+              <div className='p-4'>
+                {aboutMeSkills.map((skill) => (
                   <Badge key={skill} pill bg='light' text='dark' className='p-2 me-1'>{ skill }</Badge>
                 ))}
-                </div>
-                
               </div>
+              
             </div>
-          </Col>
-        </Row>
-      </Container>
-      </>
-    );
+          </div>
+        </Col>
+      </Row>
+    </Container>
+    </>
+  );
 }
