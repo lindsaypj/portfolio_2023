f2b43c05249f9fa0045f3ac818afa4e323e1c3e2
Patrick Lindsay <lindsaypj88@gmail.com>
2024-05-10 17:16:17 -0700

    Removed lazyloading from /portfolio hero nav cards

--- src/components/HeroNavigaion.jsx
+++ src/components/HeroNavigaion.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import { Card, Col, Row } from "react-bootstrap";
 
 import '../styles/HeroNavigation.css';
-import { LazyLoadImage } from "react-lazy-load-image-component";
+
+window.addEventListener('load', () => {})
 
 export default function HeroNavigation({ navLinks, navLinkCallback }) {
   return (
@@ -18,7 +19,7 @@ export default function HeroNavigation({ navLinks, navLinkCallback }) {
             className='nav-card mb-4 bg-black bg-opacity-75 text-white shadow border-0 overflow-hidden'
             onClick={() => {navLinkCallback(link.text)}}
           >
-            <LazyLoadImage src={link.img} alt={link.imgAlt} className='ratio-1x1' />
+            <img src={link.img} alt={link.imgAlt} className='ratio-1x1' />
             <Card.Title className='my-2 fs-md-4'>{link.text}</Card.Title>
           </Card>
         </Col>
