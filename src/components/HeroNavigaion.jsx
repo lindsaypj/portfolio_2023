42d3c4817f6a133fd961c84f43f11f36bafe6c37
Patrick Lindsay <lindsaypj88@gmail.com>
2024-05-12 22:04:24 -0700

    Dramatically reduced layout shift for /portfolio hero cards hover. #75

--- src/components/HeroNavigaion.jsx
+++ src/components/HeroNavigaion.jsx
@@ -16,7 +16,7 @@ export default function HeroNavigation({ navLinks, navLinkCallback }) {
         >
           <Card
             as={'a'}
-            className='nav-card mb-4 bg-black bg-opacity-75 text-white shadow border-0 overflow-hidden'
+            className='nav-card bg-black bg-opacity-75 text-white shadow border-0 overflow-hidden'
             onClick={() => {navLinkCallback(link.text)}}
           >
             <img src={link.img} alt={link.imgAlt} className='ratio-1x1' />
