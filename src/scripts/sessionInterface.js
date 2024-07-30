5f04f7bbab9926aa69a82839900de735285d98cf
Patrick Lindsay <lindsaypj88@gmail.com>
2024-07-30 13:48:15 -0700

    Stored/cleared path from query param in session storage

--- src/scripts/sessionInterface.js
+++ src/scripts/sessionInterface.js
@@ -6,8 +6,15 @@ export const loadSessionPageData = () => {
   const defaultPage = '';
   const searchParams = new URLSearchParams(window.location.search);
 
+  // Handle path inside query param
   if (searchParams.has('path')) {
-    return {currentPage: searchParams.get('path')};
+    const pageData = {currentPage: searchParams.get('path')};
+
+    const currentPageJson = JSON.stringify(pageData.currentPage);
+    sessionStorage.setItem(SESSION_KEYS.CURRENT_PAGE, currentPageJson);
+    window.history.pushState({}, document.title, window.location.pathname);
+    
+    return pageData;
   }
   
   const currentPageJson = sessionStorage.getItem(SESSION_KEYS.CURRENT_PAGE);
