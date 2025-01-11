8cf690ca54a0d62c2215cd4824ff450f6fb957e9
Patrick Lindsay <lindsaypj88@gmail.com>
2025-01-11 12:37:13 -0800

    Changed the order of Advise IT and Sudoku in Portfolio

--- src/views/WebDev.jsx
+++ src/views/WebDev.jsx
@@ -116,80 +116,10 @@ export default function WebDev({ scrollRef, mobileMode }) {
             />
           </Col>
         </Row>
-
-        {/* Sudoku */}
-        <Row className='portfolio__row padding-margins d-flex'>
-          <Col className='py-4 px-0 col-12 col-xl-4 d-flex align-items-end align-items-xl-center order-1 order-xl-2'>
-            <div>
-              <h3>Sudoku In the Clouds</h3>
-
-              <TruncatedArticle
-                uniqueID={'SudokuDescription'}
-                visibleParagraph={
-                  <p>
-                    Powered by my own <a href='https://github.com/lindsaypj/Sudoku-API' target='_blank' rel='noreferrer'>Sudoku API</a>,
-                    I designed a front-end application
-                    called <a href='https://github.com/lindsaypj/Sudoku-in-the-Clouds' target='_blank' rel='noreferrer'>Sudoku In the Clouds</a>.
-                    You can play 4x4, 9x9, or 16x16 board
-                    sizes. With an account, you can also save your settings, preferences, and game progress.
-                  </p>
-                }
-                truncatedParagraphs={
-                  <p>
-                    In addition to the Sudoku API that enables the gameplay, I used an API
-                    called <a href='http://colormind.io/' target='_blank' rel='noreferrer'>Colormind</a> that
-                    generates themes using color theory. These colors are used to style the
-                    application and are stored in the user's preferences.
-                  </p>
-                }
-              />
-
-              <div className='text-center m-4'>
-                <ColormindLogo />
-                <ReactLogo className='portfolio-logo' />
-                <SpringLogo className='portfolio-logo' />
-                <a href='https://cloud.google.com/' target='_blank' rel='noreferrer' title='Google Cloud Platform' >
-                  <img src={GoogleCloudLogo} className='portfolio-logo h-100' alt='Google Cloud Platform' />
-                </a>
-              </div>
-            </div>
-          </Col>
-          <Col className='p-0 py-xl-4 pe-xl-5 col-12 col-xl-8 d-flex align-items-center order-2 order-xl-1'>
-            <PortfolioCarousel
-              desktopImages={[
-                {
-                  src: SudokuHome, placeholder: SudokuHomePlaceholder,
-                  alt: 'Sudoku In the Clouds home page with Menu, Login, and Play buttons.',
-                  ratio: 'ar-640-321'
-                },
-                {
-                  src: SudokuLogin, placeholder: SudokuLoginPlaceholder,
-                  alt: 'Login page with an option to create a free account. With an account, you can track completed games and save game preferences.',
-                  ratio: 'ar-640-321'
-                },
-                {
-                  src: SudokuAccount, placeholder: SudokuAccountPlaceholder,
-                  alt: 'Example account page. Review and edit player stats, settings, and preferences.',
-                  ratio: 'ar-640-321'
-                },
-                {
-                  src: SudokuAccountThemed, placeholder: SudokuAccountThemedPlaceholder,
-                  alt: 'Example accunt page with custom prefernces that change the colors of the page.',
-                  ratio: 'ar-640-321'
-                },
-                {
-                  src: SudokuGame, placeholder: SudokuGamePlaceholder,
-                  alt: 'An partially complete game of Sudoku. The page colors match the custom preferences.',
-                  ratio: 'ar-640-321'
-                }
-              ]}
-            />
-          </Col>
-        </Row>
         
         {/* Advise It */}
         <Row className='portfolio__row padding-margins d-block d-xl-flex'>
-          <Col className='py-4 px-0 col-12 col-xl-4 d-flex align-items-center'>
+          <Col className='py-4 px-0 col-12 col-xl-4 d-flex align-items-end align-items-xl-center order-1 order-xl-2 align-items-center'>
             <div>
               <h3>
                 Advise IT
@@ -230,7 +160,7 @@ export default function WebDev({ scrollRef, mobileMode }) {
               </div>
             </div>
           </Col>
-          <Col className='p-0 py-xl-4 ps-xl-5 col-12 col-xl-8 d-flex align-items-center'>
+          <Col className='p-0 py-xl-4 pe-xl-5 col-12 col-xl-8 d-flex align-items-center order-2 order-xl-1'>
             <PortfolioCarousel
               desktopImages={[
                 {
@@ -252,6 +182,76 @@ export default function WebDev({ scrollRef, mobileMode }) {
                   src: AdviseItFooterLinks, placeholder: AdviseItFooterLinksPlaceholder,
                   alt: 'Table of footer links that can be edited or deleted. New links can be added.',
                   ratio: 'ar-3103-1907'
+                }
+              ]}
+            />
+          </Col>
+        </Row>
+
+        {/* Sudoku */}
+        <Row className='portfolio__row padding-margins d-flex'>
+          <Col className='py-4 px-0 col-12 col-xl-4 d-flex align-items-center'>
+            <div>
+              <h3>Sudoku In the Clouds</h3>
+
+              <TruncatedArticle
+                uniqueID={'SudokuDescription'}
+                visibleParagraph={
+                  <p>
+                    Powered by my own <a href='https://github.com/lindsaypj/Sudoku-API' target='_blank' rel='noreferrer'>Sudoku API</a>,
+                    I designed a front-end application
+                    called <a href='https://github.com/lindsaypj/Sudoku-in-the-Clouds' target='_blank' rel='noreferrer'>Sudoku In the Clouds</a>.
+                    You can play 4x4, 9x9, or 16x16 board
+                    sizes. With an account, you can also save your settings, preferences, and game progress.
+                  </p>
+                }
+                truncatedParagraphs={
+                  <p>
+                    In addition to the Sudoku API that enables the gameplay, I used an API
+                    called <a href='http://colormind.io/' target='_blank' rel='noreferrer'>Colormind</a> that
+                    generates themes using color theory. These colors are used to style the
+                    application and are stored in the user's preferences.
+                  </p>
+                }
+              />
+
+              <div className='text-center m-4'>
+                <ColormindLogo />
+                <ReactLogo className='portfolio-logo' />
+                <SpringLogo className='portfolio-logo' />
+                <a href='https://cloud.google.com/' target='_blank' rel='noreferrer' title='Google Cloud Platform' >
+                  <img src={GoogleCloudLogo} className='portfolio-logo h-100' alt='Google Cloud Platform' />
+                </a>
+              </div>
+            </div>
+          </Col>
+          <Col className='p-0 py-xl-4 ps-xl-5 col-12 col-xl-8 d-flex align-items-center order-2 order-xl-1'>
+            <PortfolioCarousel
+              desktopImages={[
+                {
+                  src: SudokuHome, placeholder: SudokuHomePlaceholder,
+                  alt: 'Sudoku In the Clouds home page with Menu, Login, and Play buttons.',
+                  ratio: 'ar-640-321'
+                },
+                {
+                  src: SudokuLogin, placeholder: SudokuLoginPlaceholder,
+                  alt: 'Login page with an option to create a free account. With an account, you can track completed games and save game preferences.',
+                  ratio: 'ar-640-321'
+                },
+                {
+                  src: SudokuAccount, placeholder: SudokuAccountPlaceholder,
+                  alt: 'Example account page. Review and edit player stats, settings, and preferences.',
+                  ratio: 'ar-640-321'
+                },
+                {
+                  src: SudokuAccountThemed, placeholder: SudokuAccountThemedPlaceholder,
+                  alt: 'Example accunt page with custom prefernces that change the colors of the page.',
+                  ratio: 'ar-640-321'
+                },
+                {
+                  src: SudokuGame, placeholder: SudokuGamePlaceholder,
+                  alt: 'An partially complete game of Sudoku. The page colors match the custom preferences.',
+                  ratio: 'ar-640-321'
                 }
               ]}
             />
