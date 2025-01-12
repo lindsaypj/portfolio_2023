7eb0039cde3b8755a27801ebcd2af26ef354f85a
Patrick Lindsay <lindsaypj88@gmail.com>
2025-01-12 14:12:10 -0800

    Changed Portfolio hero nav cards to squares

--- src/components/HeroNavigaion.jsx
+++ src/components/HeroNavigaion.jsx
@@ -16,11 +16,11 @@ export default function HeroNavigation({ navLinks, navLinkCallback }) {
         >
           <Card
             as={'a'}
-            className='nav-card bg-black bg-opacity-75 text-white shadow border-0 overflow-hidden'
+            className='nav-card text-white bg-black shadow border-0 overflow-hidden ratio-1x1'
             onClick={() => {navLinkCallback(link.text)}}
           >
-            <img src={link.img} alt={link.imgAlt} className='ratio-1x1' />
-            <Card.Title className='my-2 fs-md-4'>{link.text}</Card.Title>
+            <img src={link.img} alt={link.imgAlt} className='hero-nav-img' />
+            <Card.Title className='py-2 m-0 fs-md-4 bg-black'>{link.text}</Card.Title>
           </Card>
         </Col>
       ))}
